b840ddb911c6470752d2da04d4daa33e2d4ffc5e
harunkleoglu <78872745+harunkleoglu@users.noreply.github.com>
2021-10-22 21:06:33 +0300

    Create Solution04.java

---
+++ Solution04.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Solution04{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double edge = input.nextDouble();
+        double perimeter = 4*edge;
+        
+        System.out.println("Perimeter of a square with side "+edge+" is "+perimeter+".");
+
+    }
+}
