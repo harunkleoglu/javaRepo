55545c25fe6fc90f14e16230e186bf53c42c624c
harunkleoglu <78872745+harunkleoglu@users.noreply.github.com>
2021-10-22 20:47:35 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Solution{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float a = input.nextFloat();
+        float b = input.nextFloat();
+
+        System.out.println(a*b);
+    }
+}
